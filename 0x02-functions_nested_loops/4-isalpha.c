b45f5cbfaed4af63bd38527d09431b441e7ae957
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-08 10:19:51 -0700

    FEAT: Added 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Function prototype
+ * Description: Checks if a character is a letter
+ * @c: The ascii character to be checked
+ * Return: 1 if it is a letter otherwise 0
+ */
+int _alpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
