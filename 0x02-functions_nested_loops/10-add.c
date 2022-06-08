4c2e331015a30b8eb3a8b301cf3c0c0b1d38de66
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-08 11:24:50 -0700

    Corrcted 10 to print n

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * add - Function prototype
- * Description: Adds two intergers together
- * @x: The first number to add
- * @y: The second number to add
- * Return: The sum of x and y
+ * print_to_98 - Function prototype
+ * Description: Prints the number from n to 98 intergers
+ * @n: The number to start from
+ * Return: void
  */
-int add(int x, int y)
+void print_to_98(int n)
 {
-	return (x + y);
+
+	if (n <= 98)
+		for (; n <= 97; n++)
+		{
+			printf("%d, ", n);
+		}
+	else
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+
+	printf("98\n");
 }
