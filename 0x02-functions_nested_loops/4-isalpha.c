d296ec8a0fb494c953714088ca7333c37ceb41f6
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-08 10:23:28 -0700

    FIX: Corrected function name

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * @c: The ascii character to be checked
  * Return: 1 if it is a letter otherwise 0
  */
-int _alpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
