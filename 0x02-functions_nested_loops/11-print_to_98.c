a578e813bf6a80db4bb121264681130a09715e35
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-08 11:22:46 -0700

    FEAT: Added 10

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Function prototype
+ * Description: Prints the number from n to 98 intergers
+ * @n: The number to start from
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		for (n = n; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			printf("%d, n", n);
+		}
+	}
+}
