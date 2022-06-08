a6e11d56ea3d2e20a1fdb826287106a911dde388
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-08 10:49:12 -0700

    FEAT: Added 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Function prototype
+ * Description: Prints all the minutes of the day OF Jack Bauer in the
+ * format HH:MM, starting with 00:00 and ending with 23:59
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
