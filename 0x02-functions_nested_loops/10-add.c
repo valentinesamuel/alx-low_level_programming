58c60f6e68eab5536f34679fdd21e60afba81416
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-08 11:14:25 -0700

    FEAT: Aded 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Function prototype
+ * Description: Adds two intergers together
+ * @x: The first number to add
+ * @y: The second number to add
+ * Return: The sum of x and y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
