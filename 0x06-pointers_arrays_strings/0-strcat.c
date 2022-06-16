a3b5258511b6e0fd1c320bc5a5760b38a3272e28
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-16 13:54:46 -0700

    Added 00

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - appends src string to dest string
+ * @dest: string to output
+ * @src: string to be appended
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int lengthOfDestString;
+	int iterator=0;
+
+	while (dest[iterator] != '\0')
+	{
+		iterator++;
+	}
+	lengthOfDestString = iterator;
+	for (iterator = 0; src[iterator]; iterator++)
+	{
+		dest[lengthOfDestString++] = src[iterator];
+	}
+	return (dest);
+}
