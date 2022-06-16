94a1cc596383a7581e9b8781a465dce88422c55c
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-16 14:46:54 -0700

    added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,20 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int iterator;
+	int i, diff;
 
-	while (s1[iterator] != '\0' && s2[iterator] != '\0')
+	for (i = 0; s1[i] != '\0'; ++i)
 	{
-		if (s1[iterator] != s2[iterator])
+		if (s1[i] > s2[i])
 		{
-			return (s1[iterator] - s2[iterator]);
+			diff = s1[i] - s2[i];
+			return (diff);
 		}
-		iterator++;
+		else if (s1[i] < s2[i])
+		{
+			diff = s1[i] - s2[i];
+			return (diff);
+		}
 	}
 	return (0);
 }
