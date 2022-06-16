2070e4c9553468f03f4ae15aff53943c31d9ef75
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-16 14:15:53 -0700

    FEAT: Added 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function which compare two strings and
+ *@s1: first string
+ *@s2:second string
+ *Return:
+ *zero if s1 == s2
+ *negative number if s1 < s2
+ *positive number if s1 > s2
+*/
+#include "main.h"
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+	int iterator;
+
+	while (s1[iterator] != '\0' && s2[iterator] != '\0')
+	{
+		if (s1[iterator] != s2[iterator])
+		{
+			return (s1[iterator] - s2[iterator]);
+		}
+		iterator++;
+	}
+	return (0);
+}
