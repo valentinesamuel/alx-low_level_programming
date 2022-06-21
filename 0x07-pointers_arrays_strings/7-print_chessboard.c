d7bf4babb9246e92452b17932ef3df9b6299db88
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-21 04:13:48 -0700

    added 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Print a chessboard
+ * @a : Given array holding chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
