589cd2137ba476ba0c6849db16ba2d1c0492b2cb
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-21 05:01:42 -0700

    added space b4 parent

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,7 +11,7 @@ void print_diagsums(int *a, int size)
 	int sum2 = 0;
 	int i = 0;
 
-	while(i < size)
+	while (i < size)
 	{
 		sum1 = sum1 + *(a + i * size + i);
 		sum2 = sum2 + *(a + i * size * size + i - 1);
