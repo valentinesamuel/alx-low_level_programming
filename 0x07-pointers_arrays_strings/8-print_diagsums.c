aa0f4d5d06b8415c2869ff6e94fc70a0a0573a3e
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-21 04:53:22 -0700

    added 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - Print the sums of the two diagnols of a matrix
+ * @a: The matrix
+ * @size: The length of the arrays
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0;
+	int sum2 = 0;
+	int i = 0;
+
+	while(i < size)
+	{
+		sum1 = sum1 + *(a + i * size + i);
+		sum2 = sum2 + *(a + i * size * size + i - 1);
+		i++;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
