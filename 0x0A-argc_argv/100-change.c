60cfe741624696e7f60604f770afdc9995c73401
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-26 00:59:09 -0700

    reduced nl

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,46 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents;
-	int coins = 0;
+	int cents, ncoins = 0;
 
-	if (argc == 2)
-	{
-		cents = atoi(argv[1]);
-		while (cents > 0)
-		{
-			if ((cents % 25) < cents)
-			{
-				cents -= 25;
-				conts++;
-			}
-			else if ((cents % 10) < cents)
-			{
-				cents -= 10;
-				coins++;
-			}
-			else if ((cents % 5) < cents)
-			{
-				cents -= 5;
-				coins++;
-			}
-			else if (cents % 2 < cents)
-			{
-				cents -= 2;
-				coins++;
-			}
-			else if (cents % 1 < cents)
-			{
-				cents -= 1;
-				coins++;
-			}
-		}
-	}
-	else
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", coins);
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
+	}
+	printf("%d\n", ncoins);
 	return (0);
 }
