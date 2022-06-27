d34413cab38f1f958d798a2c53bd74be2596eb05
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-27 14:45:17 -0700

    freed mem

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,8 +27,9 @@ int **alloc_grid(int width, int height)
 			for (i = i - 1; i >= 0; i--)
 			{
 				free(grid[i]);
-				return (NULL);
 			}
+			free(grid);
+			return (NULL);
 		}
 		for (j = 0; j < width; j++)
 		{
