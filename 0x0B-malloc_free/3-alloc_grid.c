56a37dbf29598689c84bcbfc1175c67fa91b8296
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-27 14:22:47 -0700

    added 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocates malloc'd memory to a 2-dim array
+ * @width: width of the 2-dim array
+ * @height: height of the 2-dim array
+ * Return: malloc'd memory
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **twoDimArr;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	twoDimArr = malloc(height * sizeof(int));
+	if (twoDimArr == NULL)
+	{
+		return (NULL);
+		free(twoDimArr);
+	}
+	for (i = 0; i < height; i++)
+	{
+		twoDimArr[i] = malloc(width * sizeof(int));
+		if (twoDimArr[i] == NULL)
+		{
+			return (NULL);
+			free(twoDimArr[i]);
+		}
+		for (j = 0; j < width; j++)
+		{
+			twoDimArr[i][j] = 0;
+		}
+	}
+	return (twoDimArr);
