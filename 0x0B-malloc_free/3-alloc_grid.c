01b07a380d5bb77223e05bd0cf610e31fef063dd
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-27 14:32:09 -0700

    allocate by derefernces space

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	twoDimArr = malloc(height * sizeof(*a));
+	twoDimArr = malloc(height * sizeof(*twoDimArr));
 	if (twoDimArr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
