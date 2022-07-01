a550ff33f6d5098e4fc351557ec404e2977c5771
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-01 01:12:08 -0700

    added 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory with malloc
+ * @b: number of bytes to allocate
+ * Return: malloc'd memory or exit
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+	mem = malloc(b);
+	if (mem == NULL)
+	{
+		exit (98);
+	}
+	return (mem);
+}
