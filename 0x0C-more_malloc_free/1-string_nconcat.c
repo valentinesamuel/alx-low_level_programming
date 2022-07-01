a9fb27993a9cb5e022bfef455c3265404039a659
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-01 02:54:06 -0700

    feat: added domrev1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,35 +11,35 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *newmem;
-        unsigned int length = n;
-        unsigned int i;
+	unsigned int length = n;
+	unsigned int i;
 
-        if (s1 == NULL)
-        {
-                s1 = "";
-        }
-        if (s2 == NULL)
-        {
-                s2 = "";
-        }
-        for (i = 0; s1[i]; i++)
-        {
-                length++;
-        }
-        newmem = malloc(sizeof(char) * (length + 1));
-        if (newmem == NULL)
-        {
-                return (NULL);
-        }
-        length = 0;
-        for (i = 0; s1[i]; i++)
-        {
-                newmem[length++] = s1[i];
-        }
-        for (i = 0; s2[i] && i < n; i++)
-        {
-                newmem[length++] = s2[i];
-        }
-        newmem[length] = '\0';
-        return (newmem);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		length++;
+	}
+	newmem = malloc(sizeof(char) * (length + 1));
+	if (newmem == NULL)
+	{
+		return (NULL);
+	}
+	length = 0;
+	for (i = 0; s1[i]; i++)
+	{
+		newmem[length++] = s1[i];
+	}
+	for (i = 0; s2[i] && i < n; i++)
+	{
+		newmem[length++] = s2[i];
+	}
+	newmem[length] = '\0';
+	return (newmem);
 }
