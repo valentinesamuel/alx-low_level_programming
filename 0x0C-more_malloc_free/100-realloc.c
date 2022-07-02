d6cc7b78cf881fe774ef954e0a0753893d9413e6
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-02 04:39:20 -0700

    added 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - expands an alloc'd memory block
+ * @ptr: malloc'd space to expand
+ * @old_size: size of the malloc'd memory block
+ * @new_size: size of the realloc'd memory block
+ * Return: realloc'd mem or old mem
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newmem;
+	char *unvoidnewmem;
+	char *unvoidptr;
+	unsigned int index;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+
+	newmem = realloc(ptr, new_size);
+	if (newmem == NULL)
+		return (NULL);
+	unvoidnewmem = newmem;
+	unvoidptr = ptr;
+	for (index = 0; index < old_size; index++)
+		unvoidnewmem[index] = unvoidptr[index];
+	free(ptr);
+	return (newmem);
+}
