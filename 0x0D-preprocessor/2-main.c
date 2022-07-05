55265c08af8103c699a371d74f9ab83911b7235e
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-05 09:53:54 -0700

    ✔added 02

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - preints the name of the file this program
+ * was compiled from
+ * Return: Always success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
