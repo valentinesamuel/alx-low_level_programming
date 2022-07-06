29641752dbd3304f4053fa6d3b8107cd5069502e
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-06 16:40:27 -0700

    📍 added readme and 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name to be printed
+ * @f: the function to print the name with
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
