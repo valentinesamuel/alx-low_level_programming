efe158a1cdd05c3d853bde66039aa5dd7c7dd0ed
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-14 10:08:54 -0700

    ..

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - return the list of all elements of a list
+ * @h: the list
+ * Return: number of nodes
+ **/
+size_t list_len(const list_t *h)
+{
+	size_t numOfNodes = 0;
+	while (h)
+	{
+		numOfNodes++;
+		h = h->next;
+	}
+	return (numOfNodes);
+}
