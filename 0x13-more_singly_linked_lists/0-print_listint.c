24c464655f84ef21ac83f3c29b35da118e309a0b
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-19 06:15:22 -0700

    ⚒ 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,13 +4,13 @@
 /**
  * print_listint - print all elements of a linked list
  * @h: the first node in the list
- * Return - number of nodes
+ * Return: number of nodes
 **/
 
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
-	
+
 	while (h)
 	{
 		nodes++;
