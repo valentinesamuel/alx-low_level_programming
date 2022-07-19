9039942e8f38a4b884902be773eb9eb76895989d
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-19 10:55:10 -0700

    df

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * reverse_listint - add a node at the end of a list_t list
+ * @head: pointer to the head node
+ * Return: address of the new node
+**/
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *temp, *actual;
+
+	if (*head == NULL)
+			return (NULL);
+	actual = *head;
+
+	while(actual->next != NULL)
+	{
+		temp = actual->next;
+		actual->next = temp->next;
+		temp->next = *head;
+		*head = temp;
+	}
+	return (*head);
+}
