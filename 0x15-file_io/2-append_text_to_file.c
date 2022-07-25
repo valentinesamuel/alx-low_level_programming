beff94887f323d449a7cfdf0bac97a1d73908125
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-25 07:53:30 -0700

    third commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * append_text_to_file - append text to a file
+ * @filename: name of the file to be read
+ * @text_content: content to be put in the file
+ * Return: the number of letters printed, or 0 if it failed
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int byteswritten = 0, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_APPEND, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+		byteswritten = write(fd, text_content, len);
+		if (byteswritten == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
