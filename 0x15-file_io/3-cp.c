5758b332d124f9d68350ddda2d5828ca7a1c3613
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-25 07:55:38 -0700

    fourth commit -m

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * errorMsg - Print error message
+ * @exitCode: exit code to stop
+ * @msg: the error message to be printed
+ * @fileName: the name of the file
+ */
+void errorMsg(int exitCode, const char *msg, const char *fileName)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", msg, fileName);
+	exit(exitCode);
+}
+
+/**
+ * main: entry point
+ * @argc: argument count
+ * @argv: array of argument
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to, readfile, writefile, bytesinprocess = 1024, close_from, close_to;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(argv[1],	O_RDONLY);
+	if (fd_from == -1)
+		errorMsg(98, "Error: Can't read from file", argv[1]);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+		errorMsg(99, "Error: Can't write to", argv[2]);
+	for (readfile = read(fd_from, buffer, bytesinprocess); readfile > 0; readfile = read(fd_from, buffer, bytesinprocess))
+	{
+		writefile = write(fd_to, buffer, readfile);
+		if (writefile == -1)
+			errorMsg(99, "Error: Can't write to", argv[2]);
+	}
+	if (readfile == -1)
+		errorMsg(98, "Error: Can't read from file", argv[1]);
+	close_from = close(fd_from);
+	if (close_from == -1)
+		errorMsg(100, "Error: Can't close fd", argv[1]);
+	close_to = close(fd_to);
+	if (close_to == -1)
+		errorMsg(100, "Error: Can't close fd", argv[2]);
+	return (0);
+}
